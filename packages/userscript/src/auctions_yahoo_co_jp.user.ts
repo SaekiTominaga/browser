d194df990cf7c07947c9ed2787cef671d2ed45ee
SaekiTominaga <kcsu3470+github@gmail.com>
2024-07-21 11:20:25 +0900

    .js-toggleExpand に対応

--- packages/userscript/src/auctions_yahoo_co_jp.user.ts
+++ packages/userscript/src/auctions_yahoo_co_jp.user.ts
@@ -3,15 +3,35 @@
 // @namespace   https://w0s.jp/
 // @description 出品フォームをキーボードのみで操作できるようにする
 // @author      SaekiTominaga
-// @version     1.0.1
+// @version     1.1.0
 // @match       https://auctions.yahoo.co.jp/sell/*
 // ==/UserScript==
 (() => {
 	'use strict';
 
-	for (const expandElement of document.querySelectorAll('.js-expand')) {
-		const bodyElement = expandElement.querySelector('.js-expand-body');
+	for (const expandElement of document.querySelectorAll<HTMLElement>('.js-expand')) {
+		const bodyElement = expandElement.querySelector<HTMLElement>('.js-expand-body');
 		const triggerElement = expandElement.querySelector<HTMLElement>('.js-expand-trigger');
+
+		if (bodyElement !== null && triggerElement !== null) {
+			triggerElement.tabIndex = 0;
+			triggerElement.addEventListener('keydown', (ev: KeyboardEvent) => {
+				switch (ev.key) {
+					case 'Enter': {
+						bodyElement.classList.toggle('is-close');
+						bodyElement.classList.toggle('is-open');
+
+						break;
+					}
+					default:
+				}
+			});
+		}
+	}
+
+	for (const toggleExpandElement of document.querySelectorAll<HTMLElement>('.js-toggleExpand')) {
+		const bodyElement = toggleExpandElement.querySelector<HTMLElement>('.js-toggleExpand-body');
+		const triggerElement = toggleExpandElement.querySelector<HTMLElement>('.js-toggleExpand-trigger');
 
 		if (bodyElement !== null && triggerElement !== null) {
 			triggerElement.tabIndex = 0;
